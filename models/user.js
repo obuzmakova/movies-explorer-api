7d39737eada485210283fa81d5481d823ace0d5e
Olga Buzmakova <olga.buzmakova@gmail.com>
2021-08-11 15:07:04 +0300

    add bcrypt and authErr to user schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const AuthErr = require('../errors/auth-err');
 
 const userSchema = new mongoose.Schema({
   email: {
