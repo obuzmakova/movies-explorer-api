d460d48a4b64916cef6513839b26c363c164c7b8
Olga Buzmakova <olga.buzmakova@gmail.com>
2021-08-11 15:26:45 +0300

    change type movieId

--- models/movie.js
+++ models/movie.js
@@ -53,7 +53,7 @@ const movieSchema = new mongoose.Schema({
     required: true,
   },
   movieId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     required: true,
   },
   nameRU: {
