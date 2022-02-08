43b21847961f248d0fa14827b30d69710c69ed81
Olga Buzmakova <olga.buzmakova@gmail.com>
2022-02-08 12:16:12 +0300

    fix cors

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const allErrors = require('./middlewares/errors');
 
 const { PORT = 3000, NODE_ENV, BASE_ADDR } = process.env;
 const app = express();
+const allowedCors = [
+  'localhost:3000'
+];
 
 mongoose.connect(NODE_ENV === 'production' ? BASE_ADDR : 'mongodb://localhost:27017/filmsdb', {
   useNewUrlParser: true,
@@ -20,6 +23,26 @@ mongoose.connect(NODE_ENV === 'production' ? BASE_ADDR : 'mongodb://localhost:27
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.status(200).send();
+  }
+
+  next();
+});
 
 app.use('/signin', require('./routes/signin'));
 app.use('/signup', require('./routes/signup'));
